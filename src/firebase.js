a7e81f7c0214ebcd3c380226ab31c09750e7bcbb
legazy33 <chris.jimenez50@yahoo.com>
2021-05-13 21:16:50 -0700

    initial commit - header created

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDg_513LSSQuHNGScOasOar8Zpo0sMv1bo",
+    authDomain: "facebook-d9f3c.firebaseapp.com",
+    projectId: "facebook-d9f3c",
+    storageBucket: "facebook-d9f3c.appspot.com",
+    messagingSenderId: "496667382404",
+    appId: "1:496667382404:web:fe6e643ee3d391d357a8d1",
+    measurementId: "G-EFMG641GJD"
+  };
+  
