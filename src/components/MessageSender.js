cfe359dca8a3717184b2343f86f918e13c8e52b6
legazy33 <chris.jimenez50@yahoo.com>
2021-07-22 00:58:06 -0700

    fixed message sender issue

--- src/components/MessageSender.js
+++ src/components/MessageSender.js
@@ -9,7 +9,6 @@ import db from '../firebase'
 import firebase from 'firebase'
 
 function MessageSender() {
-  const [{user}, dispatch] = useStateValue();
     const [input, setInput] = useState('');
     const [imageUrl, setImageUrl] = useState('');
 
